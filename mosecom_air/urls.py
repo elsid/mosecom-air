d5dcaacea3b7a901e956254658fd67fb69157a80
elsid <elsid.mail@gmail.com>
2014-08-25 23:49:18 +0400

    Add static urls

--- mosecom_air/urls.py
+++ mosecom_air/urls.py
@@ -1,7 +1,9 @@
 #coding: utf-8
 
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^api/', include('mosecom_air.api.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
