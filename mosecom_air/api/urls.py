3c899c61d5a814d3237e6d2e15744491bdc48d2c
elsid <elsid.mail@gmail.com>
2014-08-28 23:22:25 +0400

    Resctrict urls

--- mosecom_air/api/urls.py
+++ mosecom_air/api/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import patterns, url
 from mosecom_air.api import views
 
 urlpatterns = patterns('',
-    url(r'^add', views.add, name='add'),
-    url(r'^measurements', views.measurements, name='measurements'),
-    url(r'^ping', views.ping, name='ping'),
-    url(r'^stations', views.stations, name='stations'),
-    url(r'^substances', views.substances, name='substances'),
-    url(r'^units', views.units, name='units'),
-    url(r'^update', views.update, name='update'),
+    url(r'^add$', views.add, name='add'),
+    url(r'^measurements$', views.measurements, name='measurements'),
+    url(r'^ping$', views.ping, name='ping'),
+    url(r'^stations$', views.stations, name='stations'),
+    url(r'^substances$', views.substances, name='substances'),
+    url(r'^units$', views.units, name='units'),
+    url(r'^update$', views.update, name='update'),
 )
