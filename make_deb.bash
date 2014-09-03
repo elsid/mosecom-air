0359ea731f8055f619934773ecc77ab08bd31481
elsid <elsid.mail@gmail.com>
2014-09-04 01:09:16 +0400

    Add nginx-extras package dependency

--- make_deb.bash
+++ make_deb.bash
@@ -11,6 +11,7 @@ cd deb_dist/mosecom-air-$VERSION &&
 sed -i 's/Package: python-mosecom-air/Package: mosecom-air/' debian/control &&
 sed -i 's/python-all/python/' debian/control &&
 sed -iE 's/Depends:.*/\0, libossp-uuid-perl/' debian/control &&
+sed -iE 's/Depends:.*/\0, nginx-extras/' debian/control &&
 echo '
 override_dh_auto_install:
 	python setup.py install --root=debian/mosecom-air --install-layout=deb \
