01ddc64aad8f6e209ecb1a0d1bf3cf35a7ba8f39
elsid <elsid.mail@gmail.com>
2014-09-05 23:49:33 +0400

    Copy existing files instead of generation by echo in make_deb.bash

--- make_deb.bash
+++ make_deb.bash
@@ -18,30 +18,9 @@ override_dh_auto_install:
 		--install-scripts=/usr/share/mosecom-air/bin
 override_dh_auto_build:
 ' >> debian/rules &&
-echo '
-/usr/share/mosecom-air/bin/manage.py /usr/bin/mosecom-air-manage
-/usr/share/mosecom-air/bin/request.py /usr/bin/mosecom-air-request
-/usr/share/mosecom-air/bin/parse_html.py /usr/bin/mosecom-air-parse-html
-/usr/share/mosecom-air/etc/nginx.conf /etc/nginx/conf.d/mosecom-air.conf
-/usr/share/mosecom-air/etc/init.sh /etc/init.d/mosecom-air
-' > debian/links &&
-echo '
-etc/init.sh /usr/share/mosecom-air/etc
-etc/nginx.conf /usr/share/mosecom-air/etc
-' > debian/install &&
-echo '
-30	*	*	*	*	root	curl localhost:13710/api/update
-*	*	*	*	*	root	if [ "$(curl localhost:13710/api/ping)" != "pong" ]; then service mosecom-air restart; fi
-' > debian/mosecom-air.cron.d
-echo '
-mkdir -p /var/log/mosecom-air &&
-mkdir -p /var/log/nginx/mosecom-air && {
-	/etc/init.d/mosecom-air stop
-	/etc/init.d/mosecom-air start
-	true
-}
-' > debian/postinst &&
-echo '
-/etc/init.d/mosecom-air stop
-' > debian/prerm &&
+cp ../../../debian/cron.d debian/mosecom-air.cron.d &&
+cp ../../../debian/install debian/ &&
+cp ../../../debian/links debian/ &&
+cp ../../../debian/postinst debian/ &&
+cp ../../../debian/prerm debian/ &&
 debuild
