06558b8eaadaf0bb6026836928e69f331d544f97
elsid <elsid.mail@gmail.com>
2014-09-05 23:35:49 +0400

    Fix regexps to change debian/control in make_deb.bash

--- make_deb.bash
+++ make_deb.bash
@@ -8,10 +8,10 @@ python setup.py sdist &&
 cd dist &&
 py2dsc $NAME-$VERSION.tar.gz &&
 cd deb_dist/mosecom-air-$VERSION &&
-sed -i 's/Package: python-mosecom-air/Package: mosecom-air/' debian/control &&
+sed -i 's/^Package: python-mosecom-air/Package: mosecom-air/' debian/control &&
 sed -i 's/python-all/python/' debian/control &&
-sed -iE 's/Depends:.*/\0, libossp-uuid-perl/' debian/control &&
-sed -iE 's/Depends:.*/\0, nginx-extras/' debian/control &&
+sed -iE 's/^Depends:.*/\0, libossp-uuid-perl/' debian/control &&
+sed -iE 's/^Depends:.*/\0, nginx-extras/' debian/control &&
 echo '
 override_dh_auto_install:
 	python setup.py install --root=debian/mosecom-air --install-layout=deb \
