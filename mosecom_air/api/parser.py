978b9450346f4de687ed3c23bc11c970538e948b
elsid <elsid.mail@gmail.com>
2015-01-17 21:55:17 +0300

    Use tuple in namedtuples initization

--- mosecom_air/api/parser.py
+++ mosecom_air/api/parser.py
@@ -2,6 +2,8 @@
 
 from collections import namedtuple
 
-Substance = namedtuple('Substance', 'name alias')
-Measurement = namedtuple('Measurement', 'substance unit performed value')
-Result = namedtuple('Result', 'measurements substances units station_alias')
+Substance = namedtuple('Substance', ('name', 'alias'))
+Measurement = namedtuple('Measurement', ('substance', 'unit', 'performed',
+                                         'value'))
+Result = namedtuple('Result', ('measurements', 'substances', 'units',
+                               'station_alias'))
