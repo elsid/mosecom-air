f1decf0244e0fb5b4be4d6ef106d5278590cb9f3
elsid <elsid.mail@gmail.com>
2015-04-20 01:00:47 +0300

    Fix cron config

--- debian/cron.d
+++ debian/cron.d
@@ -1,2 +1,2 @@
-30	*	*	*	*	root	curl localhost:13710/api/update
-*	*	*	*	*	root	curl -s localhost:13710/api/ping | fgrep ok || service mosecom-air restart
+30	*	*	*	*	root	curl -s localhost:13710/api/update | fgrep -v '{"status": "ok"}'
+*	*	*	*	*	root	curl -s localhost:13710/api/ping | fgrep -v '{"status": "ok"}' && service mosecom-air restart
