909a44f12cacff6568a876d40a2ab3f72f350fb5
elsid <elsid.mail@gmail.com>
2015-06-12 12:05:31 +0300

    [1.10.7] Fix cron.d

--- debian/cron.d
+++ debian/cron.d
@@ -1,2 +1,2 @@
-30	*	*	*	*	root	curl -s localhost:13710/api/update | fgrep -v '{"status": "ok"}'
+30	*	*	*	*	root	curl -s localhost:13710/api/update | fgrep -v '{"status": "done"}'
 *	*	*	*	*	root	curl -s localhost:13710/api/ping | fgrep -v '{"status": "ok"}' && service mosecom-air restart
